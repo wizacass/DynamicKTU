f551228e9fc53a873df0a759baa4475f953b44d0
Wizacass <visak.pet0@gmail.com>
2020-06-01 17:43:09 +0300

    Implement time logging

--- Program.cs
+++ Program.cs
@@ -1,22 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace Formule
 {
     internal class Program
     {
+        private const string CsvFilePattern = "Logs/log {0}.csv";
+        private const int Generations = 16;
+
+        private static Func<int, int>[] Functions = {
+                Function.Recursion,
+                Function.Dynamic,
+                Function.Parallel
+            };
+
+        private static int CalculateEntries(int i)
+        {
+            //return 10 * (int)Math.Pow(2, i);
+            return (int)Math.Pow(2, i);
+        }
+
         private static void Main(string[] args)
         {
-            Console.WriteLine("Recursion vs Dynamic");
-            for (int i = 1; i <= 40; i++)
+            string path = string.Format(CsvFilePattern, DateTime.Now.ToString().Replace('/', '-')).Replace(':', '.');
+            using var csvWriter = new StreamWriter(path);
+            for (int i = 0; i <= Generations; i++)
             {
-                int rec = Function.Recursion(i);
-                int dyn = Function.Dynamic(i);
-                int par = Function.Parallel(i);
-                string rvd =
-                    $"{i,3}. {rec,4} {(rec == dyn ? "==" : "!=")} {dyn,-4} {(rec != dyn ? $"Diff: {rec - dyn}" : "")}";
-                string rvp =
-                    $"{i,3}. {rec,4} {(rec == par ? "==" : "!=")} {par,-4} {(rec != par ? $"Diff: {rec - par}" : "")}";
-                Console.WriteLine($"{rvd}\t{rvp}");
+                Console.WriteLine($"Running {i}/{Generations} calculation...");
+                var results = new List<string>();
+                int x = CalculateEntries(i);
+                foreach (var func in Functions)
+                {
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+
+                    var sw = new Stopwatch();
+                    sw.Start();
+                    int result = func(x);
+                    sw.Stop();
+
+                    results.Add(sw.ElapsedTicks.ToString());
+                }
+
+                csvWriter.WriteLine(results.Aggregate($"{x};", (current, result) => current + $"{result};"));
             }
         }
     }
